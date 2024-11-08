43943af72027d5e713d5717d5b6dd4baf945618e
Adam Sidor <sidoadsi1@gmail.com>
2024-11-08 20:34:07 +0100

    Initial Commit

---
+++ ADMleds/ADMledsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADMledsApp.swift
+//  ADMleds
+//
+//  Created by Adam Sidor on 08/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ADMledsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
