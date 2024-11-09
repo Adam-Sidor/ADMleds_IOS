1b2a83ceef6fa9e7dd9813c89d8c8e69f522541e
Adam Sidor <sidoadsi1@gmail.com>
2024-11-09 17:22:36 +0100

    ColorSet added and some other changes

---
+++ ADMleds/GeneralSetView.swift
@@ -0,0 +1,18 @@
+//
+//  GeneralSetView.swift
+//  ADMleds
+//
+//  Created by Adam Sidor on 09/11/2024.
+//
+
+import SwiftUI
+
+struct GeneralSetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GeneralSetView()
+}
